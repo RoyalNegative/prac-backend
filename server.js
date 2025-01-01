cf06e72275e1faf5a32db076bdc2a0558fbc3bb9
Kaan <103496597+RoyalNegative@users.noreply.github.com>
2025-01-01 23:04:26 +0300

    changes

--- server.js
+++ server.js
@@ -2,20 +2,18 @@ import OpenAI from 'openai';
 
 
 const express = require('express');
-const cors = require('cors'); 
+const cors = require('cors');
 const app = express();
+
 
 app.use(cors());
 
-app.get('/', (req, res) => {
-  res.send('Hello, Heroku!');
-});
-
 app.get('/api/message', (req, res) => {
-  res.send('Hello, this is the api message!');
+  res.send('This is a message from the backend!');
 });
 
-
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
